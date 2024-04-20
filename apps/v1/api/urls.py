dca6603bc2c60417f1f2350f667da484252eab29
Alexander <akrimec@gmail.com>
2024-04-20 23:41:59 +0300

    v1 parser created

--- apps/v1/api/urls.py
+++ apps/v1/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
+from apps.v1.views import parse_website_view
 
 
 # /api/v1/...
 
 urlpatterns = [
-
+    path('parse-website/', parse_website_view, name='parse_website'),
 ]
