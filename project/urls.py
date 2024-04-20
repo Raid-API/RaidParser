c4563e747fa9b734b28dbc3c3c4cc70e3f634406
Alexander <akrimec@gmail.com>
2024-04-20 22:52:30 +0300

    index added

--- project/urls.py
+++ project/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path, re_path
+from apps.v1.views import index
+
 
 urlpatterns = [
     re_path(r'^api/', include('api.urls')),
     path('admin/', admin.site.urls),
+    path('', index, name='index'),
 ]
